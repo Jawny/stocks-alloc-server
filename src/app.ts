86e55778f550090b22a1d771e3abd1d16794eda9
Johnny Liao <johnnyliao06@gmail.com>
2021-04-16 22:28:27 -0700

    added .env file

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
 import "module-alias/register";
 import express from "express";
 import { yahooApiRoutes } from "@routes";
+require("dotenv").config();
 
+const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api", yahooApiRoutes);
 
-app.listen(3000, () => {
-  console.log("server listening on port 3000!");
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
 });
