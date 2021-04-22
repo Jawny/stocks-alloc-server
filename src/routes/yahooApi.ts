3c14fd2867842e21a5074d2f1fb9df95cb9bfcfb
Johnny Liao <johnnyliao06@gmail.com>
2021-04-21 23:34:06 -0700

    fixed api routes by using req.query for GET req

--- src/routes/yahooApi.ts
+++ src/routes/yahooApi.ts
@@ -22,27 +22,25 @@ const yahooApiRoute = express.Router();
  * @memberof module:routes/yahooApi~Yahoo Api
  * @inner
  * @see {@link https://github.com/gadicc/node-yahoo-finance2/blob/devel/docs/modules/quoteSummary.md#module-options List of option strings for modules param.}
- * @param {Object} req.body - Request body.
- * @param {string} req.body.ticker - Stock ticker symbol.
- * @param {Array<string>} req.body.modules - Array of module option strings.
+ * @param {Object} req.query - Request parameters.
+ * @param {string} req.query.ticker - Stock ticker symbol.
+ * @param {Array<string>} req.query.modules - Array of module option strings.
  * @return {Object} - Object with module summary details.
  */
-yahooApiRoute.get("/quote-summary", async (req, res) => {
-  const { ticker, modules } = req.body;
-
-  if (!ticker || !modules) {
-    return res.status(400).json("missing paramaters: ticker, modules.");
-  }
-
+yahooApiRoute.get("/quote-summary", async (req: any, res) => {
   try {
-    const yahooSummary = await yahooFinance.quoteSummary(ticker, {
-      modules,
-    });
-    return res.status(200).json(yahooSummary);
-  } catch (error) {
-    return res
-      .status(400)
-      .json("invalid parameters. Modules string array is case sensitive.");
+    const { ticker, modules } = req.query;
+
+    if (!ticker || !modules) {
+      res.status(400).json("missing parameters: ticker, modules.");
+    } else {
+      const yahooSummary = await yahooFinance.quoteSummary(ticker, {
+        modules,
+      });
+      res.status(200).json(yahooSummary);
+    }
+  } catch (err) {
+    res.status(400).json("invalid parameters.");
   }
 });
 
